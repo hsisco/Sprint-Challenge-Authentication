14fff330dd4e45d5b1bac0e7a21629e03740bac1
hsisco <hysensisco@gmail.com>
2020-05-03 21:58:06 -0400

    server + auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,35 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('../user/user-model.js');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+  
+  Users.add(user)
+    .then(saved => res.status(201).json(saved))
+    .catch(err => res.status(500).json(err));
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+  console.log('login')
+  
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      console.log('login findBy');
+      
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = username;
+        res.status(200).json({ message: `Welcome ${user.username}!`, token });
+      } else {
+        res.status(401).json({ message: 'You shall not pass!' });
+      }
+      })
+      .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
